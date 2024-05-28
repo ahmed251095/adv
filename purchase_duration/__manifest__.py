e4415985156f6fcb280ce4b332f9847c201d0fce
mahmoud ezzat <37959194+mahmmoudezzat@users.noreply.github.com>
2024-05-28 04:09:58 +0300

    purchase duration module

---
+++ purchase_duration/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Purchase Duration",
+    "version": "16.0.1.0.1",
+    "author": "mahmoud ezzat",
+    "website": "https://github.com/OCA/stock-logistics-availability",
+    "development_status": "Production/Stable",
+    "category": "Warehouse",
+    "depends": ["sale"],
+    "license": "AGPL-3",
+    "data": [
+        "views/purchase_quant.xml",
+    ],
+    "installable": True,
+}
---
+++ purchase_duration/models/purchase_quant.py
@@ -0,0 +1,19 @@
+from odoo import api, fields, models
+
+class SaleOrderLineInheritTime(models.Model):
+
+    _inherit = "sale.order.line"
+
+    duration = fields.Float(string="المدة",default=1)
+    product_price = fields.Float(related='product_id.list_price')
+    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id','duration')
+    def _compute_amount(self):
+        """
+        Compute the amounts of the SO line.
+        This is the method where price computation is typically done.
+        """
+        super(SaleOrderLineInheritTime, self)._compute_amount()
+        for line in self:
+            line.price_unit = line.product_id.list_price
+            if line.duration:
+                line.price_unit = line.price_unit * line.duration
